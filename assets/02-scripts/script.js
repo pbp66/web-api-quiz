2e1ea28ad8bbbfff154e49db2882b5e1d9d15a3e
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-09-23 23:30:44 -0400

    Moved the question class and question loader over to loading-script.js

--- assets/02-scripts/script.js
+++ assets/02-scripts/script.js
@@ -1,26 +1,26 @@
 /* Question Class Declaration */
-class Question {
-    constructor(title = "", option_1 = "", option_2 = "", option_3 = "", option_4 = "", answer = "") {
-        this.title = title;
-        this.option_1 = option_1;
-        this.option_2 = option_2;
-        this.option_3 = option_3;
-        this.option_4 = option_4;
-        this.answer = answer;
-    }
-};
+// class Question {
+//     constructor(title = "", option_1 = "", option_2 = "", option_3 = "", option_4 = "", answer = "") {
+//         this.title = title;
+//         this.option_1 = option_1;
+//         this.option_2 = option_2;
+//         this.option_3 = option_3;
+//         this.option_4 = option_4;
+//         this.answer = answer;
+//     }
+// };
 
-var questionList = [];
+// var questionList = [];
 
-async function loadQuestions() {
-    var response = await fetch("./assets/04-data/questions.json").then(response => response.json());
-    var questions = Object.keys(response);
-    for (var i = 0; i < questions.length; i++ ) {
-        var temp = new Question();
-        temp = response[questions[i]];
-        questionList.push(temp);
-    }
-};
+// async function loadQuestions() {
+//     var response = await fetch("./assets/04-data/questions.json").then(response => response.json());
+//     var questions = Object.keys(response);
+//     for (var i = 0; i < questions.length; i++ ) {
+//         var temp = new Question();
+//         temp = response[questions[i]];
+//         questionList.push(temp);
+//     }
+// };
 
 // console.log(response[questions[i]]);
 // console.log(questionList[i].title);
