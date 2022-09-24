c3fc4ebd74bfa904bccd3b18604065ced353e316
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-09-23 23:57:47 -0400

    Added some more dev tests to verify that the questions are loading correctly

--- assets/02-scripts/script.js
+++ assets/02-scripts/script.js
@@ -1,45 +1,42 @@
 /* Question Class Declaration */
-// class Question {
-//     constructor(title = "", option_1 = "", option_2 = "", option_3 = "", option_4 = "", answer = "") {
-//         this.title = title;
-//         this.option_1 = option_1;
-//         this.option_2 = option_2;
-//         this.option_3 = option_3;
-//         this.option_4 = option_4;
-//         this.answer = answer;
-//     }
-// };
+class Question {
+    constructor(title = "", option_1 = "", option_2 = "", option_3 = "", option_4 = "", answer = "") {
+        this.title = title;
+        this.option_1 = option_1;
+        this.option_2 = option_2;
+        this.option_3 = option_3;
+        this.option_4 = option_4;
+        this.answer = answer;
+    }
+};
 
-// var questionList = [];
+var questionList = [];
 
-// async function loadQuestions() {
-//     var response = await fetch("./assets/04-data/questions.json").then(response => response.json());
-//     var questions = Object.keys(response);
-//     for (var i = 0; i < questions.length; i++ ) {
-//         var temp = new Question();
-//         temp = response[questions[i]];
-//         questionList.push(temp);
-//     }
-// };
+async function loadQuestions(event) {
+    event.stopPropagation();
+    var questionData = await fetch("./assets/04-data/questions.json").then(response => response.json());
+    var questions = Object.keys(questionData);
+    for (var i = 0; i < questions.length; i++ ) {
+        var temp = new Question();
+        temp = questionData[questions[i]];
+        questionList.push(temp);
+    }
+    console.log(questionList);
+};
 
 // console.log(response[questions[i]]);
 // console.log(questionList[i].title);
 
 function randomizeList(list) {
-    console.log(list);
     var temp;
     for (var i = 0; i < list.length; i++) {
         var random1 = randomInt(list.length);
         var random2 = randomInt(list.length);
-        console.log(random1);
-        console.log(random2);
-        
         temp = list[random1];
         list[random1] = list[random2];
         list[random2] = temp;
     }
-    console.log(list);
-    return list;
+    console.log(questionList);
 }
 
 function randomInt(range) {
@@ -49,7 +46,7 @@ function randomInt(range) {
 
 /* DEV TEST SECTION */
 var test = document.querySelector("#answer-1");
-
 test.addEventListener("click", loadQuestions);
 
-questionList = randomizeList(questionList);
+var test2 = document.querySelector("#answer-4");
+test2.addEventListener("click", () => randomizeList(questionList));
