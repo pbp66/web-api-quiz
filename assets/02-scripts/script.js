2cc214a1801888ec37eb68548e46d4b148374081
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-09-23 21:21:16 -0400

    Added question object constructor

---
+++ assets/02-scripts/script.js
@@ -0,0 +1,14 @@
+/* Question Class Declaration */
+
+function Question(title = "", option_1 = "", option_2 = "", option_3 = "", option_4 = "", answer = "") {
+    this.title = title;
+    this.option_1 = option_1;
+    this.option_2 = option_2;
+    this.option_3 = option_3;
+    this.option_4 = option_4;
+    this.answer = answer;
+};
+
+function loadQuestions(filename) {
+    fetch("./assets./04-data./questions.json")
+};
