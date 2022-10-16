edeaba7cfbb540896b2d6188f8e282b23cf3bbd5
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-16 15:58:08 -0400

    Added empty functions as roadmap

--- assets/02-scripts/scores-script.js
+++ assets/02-scripts/scores-script.js
@@ -1,1 +1,20 @@
 // Scores HTML script code
+function addScore() {
+
+}
+
+function removeScore() {
+    
+}
+
+function loadScores() {
+
+}
+
+function saveScores() {
+
+}
+
+function resetAllScores() {
+    
+}
